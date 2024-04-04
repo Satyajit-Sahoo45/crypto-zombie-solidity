899e0458ce6ca91189046cab53a3ff289977513b
Satyajit <sahoosatyajit2801@gmail.com>
2024-04-04 22:22:04 +0530

    payment in ETH

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -4,9 +4,27 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+  uint levelUpFee = 0.001 ether;
+
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
+  }
+
+  // 1. Create withdraw function here
+  function withdraw() external onlyOwner {
+    address payable _owner = address(uint160(owner()));
+    _owner.transfer(address(this).balance);
+  }
+
+  // 2. Create setLevelUpFee function here
+  function setLevelUpFee(uint _fee) external onlyOwner{
+    levelUpFee = _fee;
+  }
+
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
   }
 
   function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
@@ -21,10 +39,9 @@ contract ZombieHelper is ZombieFeeding {
 
   function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
     uint[] memory result = new uint[](ownerZombieCount[_owner]);
-    // Start here
     uint counter = 0;
-    for(uint i=0; i<zombies.length; i++){
-      if(zombieToOwner[i] == _owner){
+    for (uint i = 0; i < zombies.length; i++) {
+      if (zombieToOwner[i] == _owner) {
         result[counter] = i;
         counter++;
       }
